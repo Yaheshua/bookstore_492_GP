0c529e19aa5f9e1b09d76e7f9ae0ddbcc7f14e8a
Yaheshua <82551504+Yaheshua@users.noreply.github.com>
2024-09-29 15:08:55 -0400

    Update NewBookPage.py

--- NewBookPage.py
+++ NewBookPage.py
@@ -73,3 +73,68 @@ class NewBook(object):
         self.bookPriceLabel = Label(
             self.topFrame, text="Price", font=self.font, bg=self.color)
         self.bookPriceLabel.grid(row=6, column=0, sticky=W, padx=2, pady=2)
+
+        #creating entries
+        self.bookNameEntry = Entry(self.topFrame, font=self.font)
+        self.bookNameEntry.grid(row=1, column=1, padx=2, pady=2)
+
+        self.bookAuthorEntry = Entry(self.topFrame, font=self.font)
+        self.bookAuthorEntry.grid(row=2, column=1, padx=2, pady=2)
+
+        self.bookGenreEntry = Entry(self.topFrame, font=self.font)
+        self.bookGenreEntry.grid(row=3, column=1, padx=2, pady=2)
+
+        self.bookYearEntry = Entry(self.topFrame, font=self.font)
+        self.bookYearEntry.grid(row=4, column=1, padx=2, pady=2)
+
+        self.bookStockEntry = Entry(self.topFrame, font=self.font)
+        self.bookStockEntry.grid(row=5, column=1, padx=2, pady=2)
+
+        self.bookPriceEntry = Entry(self.topFrame, font=self.font)
+        self.bookPriceEntry.grid(row=6, column=1, padx=2, pady=2)
+
+        #creating register button
+        self.registerButton = Button(
+            self.topFrame, text="Register new Book", font=32)
+        self.registerButton.bind("<Button-1>", self.__registerBook)
+        self.registerButton.grid(row=7, column=1, columnspan=2, padx=1, pady=1)
+
+    def __registerBook(self, event):
+
+        bookName = self.bookNameEntry.get()
+        bookAuthor = self.bookAuthorEntry.get()
+        bookGenre = self.bookGenreEntry.get()
+        bookYear  =self.bookYearEntry.get()
+        bookCost = float(self.bookPriceEntry.get())
+        bookStock = int(self.bookStockEntry.get())
+
+        #check if the book exists
+        cursor = self.dbConnection.cursor()
+        args = (bookName, None)
+        result = cursor.callproc('isBook', args)
+        if result[1] != 0:
+            self.__message("Book already exists")
+        else:
+
+            args = (bookName, bookGenre, bookAuthor, bookYear, bookStock, bookCost)
+            result = cursor.callproc('insertBook', args)
+            self.dbConnection.commit()
+            self.__message("Book added successfully")
+        cursor.close()
+
+
+
+
+
+
+        for child in self.root.winfo_children():
+            child.destroy()
+
+        self.root.destroy()
+
+
+    def __message(self, message):
+        new_window = Toplevel(self.root)
+        Message.Message(new_window, self.color, message)
+        new_window.wait_window()
+
